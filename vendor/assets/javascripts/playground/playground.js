deff66ff4419c9a7e53cc9de80f4d8cc652d7260
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2013-10-16 14:55:33 +1000

    - Allow tables to be created for an array of objects. - Add onBeforeTemplate callback to allow processing just before the template is created. - Move the childTemplate processing into the parent node so we have adequate data for processing.

--- vendor/assets/javascripts/playground/playground.js
+++ vendor/assets/javascripts/playground/playground.js
@@ -198,6 +198,28 @@ $('document').ready(function () {
       };
       
       // TESTING.
+      // Create array field types - clone the defaults and set their `fieldtemplate` property to false.
+      var arrayFieldTemplates = [
+        'text',
+        'password',
+        'date',
+        'datetime',
+        'datetime-local',
+        'email',
+        'month',
+        'number',
+        'search',
+        'tel',
+        'time',
+        'url',
+        'week'
+      ];
+      _.each(arrayFieldTemplates, function(id){
+        var o = _.clone(JSONForm.fieldTypes[id])
+        o.fieldtemplate = false;
+        JSONForm.fieldTypes['table'+id] = o;
+      });
+      
       function getParentType(node){
         var type = null;
         if (node && node.parentNode && node.parentNode.schemaElement){
@@ -216,18 +238,24 @@ $('document').ready(function () {
           node.view = JSONForm.fieldTypes['tablearray'];
           console.log('!!!tablearray\ndata=', data, '\nnode=', node);
           
-        } else if (node.schemaElement.type == 'object'){
-          if (getParentType(node) == 'array'){
-            node.view = JSONForm.fieldTypes['tableobject'];
-            console.log('@@@tableobject\ndata=', data, '\nnode=', node);
-            
-          } else if (getParentType(node.parentNode) == 'array' && getParentType(node) == 'object') {
-            // The parent is an object on an array.
-            
-            // We should be a leaf node - a text input or something like that.
-            // Can we be a fieldset??
-            console.log('###node.schemaElement.type=', node.schemaElement.type);
-          }
+        } else if (node.schemaElement.type == 'object' && getParentType(node) == 'array'){
+          // Object item in an array.
+          node.view = JSONForm.fieldTypes['tableobject'];
+          console.log('@@@tableobject\ndata=', data, '\nnode=', node);
+        } else if (getParentType(node.parentNode) == 'array' && getParentType(node) == 'object') {
+          // Sub-property of the object item in an array.
+          // The parent is an object on an array.
+          
+          // Get the view type.
+          var type = node.formElement.type;
+          var newView = JSONForm.fieldTypes['table'+type];
+          node.view = newView;
+          
+          // TODO: Wrap each child in a <td></td>.
+          
+          // We should be a leaf node - a text input or something like that.
+          // Can we be a fieldset??
+          console.log('###node.schemaElement.type=', type, ', newView=', newView);
         }
         
         // node.view = null;
